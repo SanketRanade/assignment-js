6ddf417fdfb55cfaf9e64d90f67e6b6759ce7a5c
SanketRanade <54371040+SanketRanade@users.noreply.github.com>
2022-05-22 22:10:28 +0530

    added buttons

--- script.js
+++ script.js
@@ -6,21 +6,48 @@ import data from "./items.json" assert { type: "json" };
 
 // const obj = JSON.parse("./items.json");
 
-function populate(obj) {
+function populate(obj, array) {
 	const section = document.querySelector("section");
 	const myList = document.createElement("ul");
 	for (let index = 0; index < obj.length; index++) {
 		const listItem = document.createElement("li");
+		const buttonItem = document.createElement("button");
 		// listItem.style.cssText = "listStyleImage:url(sprinklr.png)";
 		// listItem.style.listStyleImage = url(sprinklr.png);
 		// listItem.style.width = 100;
 		const imageName = obj[index]["title"];
-		listItem.textContent = imageName;
+		const imageURL = obj[index]["previewImage"];
+		array.push(imageURL);
+
+		buttonItem.textContent = imageName;
+		// buttonItem.setAttribute("id", imageURL);
+		// listItem.style.backgroundImage = "url(" + imageURL + ")";
+
+		// buttonItem.style.onclick = changeImage(array[index]);
+		buttonItem.addEventListener("click", changeImage);
+		buttonItem.style.backgroundImage = "url(" + imageURL + ")";
+		listItem.appendChild(buttonItem);
 		myList.appendChild(listItem);
 	}
-	myList.style.cssText = "listStyleImage:url(sprinklr.png)";
+	// myList.style.cssText = "listStyleImage:url(sprinklr.png)";
 	// myList.style.position = inside;
-	section.appendChild(myList);
+	section.prepend(myList);
+
+	const myImage = document.querySelector("img");
+	// myImage.setAttribute("id", "mainimage");
+	myImage.src = obj[4]["previewImage"];
+	// section.prepend(myImage);
 }
 
-populate(data);
+// function changeImage(srce) {
+// 	document.getElementById("mainimage").src = srce;
+// 	// imageVar.src = srce;
+// }
+
+const changeImage = (src) => {
+	// document.getElementById("mainimage").src = src;
+	console.log("hi\n");
+};
+
+const array = [];
+populate(data, array);
